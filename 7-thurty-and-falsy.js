ff286a429f3e23760b9ec2defeb6049ef8a55049
JaredTc <JaredTc@users.noreply.github.com>
2020-12-12 00:18:35 -0600

    thurty and falsy

--- 7-thurty-and-falsy.js
+++ 7-thurty-and-falsy.js
@@ -1,1 +1,15 @@
 console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+console.log(Boolean(false));
+console.log(Boolean(""));
+
+// los verdaderos
+console.log(Boolean(1));
+console.log(Boolean("jshgds"));
+console.log(Boolean([]));
+console.log(Boolean({}));
+console.log(Boolean(function(){}));
+
+
