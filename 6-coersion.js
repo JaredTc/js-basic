e30ba4499137b007f67ea43b2b1907afb9d52be4
JaredTc <JaredTc@users.noreply.github.com>
2020-12-12 00:13:18 -0600

    implemente coersion

---
+++ 6-coersion.js
@@ -0,0 +1,13 @@
+//coersion impplicita
+
+var a = 1 + "2";
+console.log(typeof a);
+
+var b = 2 * "4";
+console.log(typeof b);
+
+//coersiion Explicita
+
+var c = 15;
+var d = c + "";
+console.log(typeof d);
